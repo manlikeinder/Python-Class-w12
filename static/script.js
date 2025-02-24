d8ec0b4111d72e18aa318b7aaf3c1eaf2bab4fc0
manlikeinder <contact.inderpreetsingh@gmail.com>
2025-02-25 12:15:42 +1300

    First commit

---
+++ static/script.js
@@ -0,0 +1,6 @@
+document.getElementById('alertBtn').addEventListener('click', function() {
+    alert('Hello from JavaScript!');
+});
+document.getElementById('secBtn').addEventListener('click', function () {
+    alert('Hello from Second Button!');
+});
